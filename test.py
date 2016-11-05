022f0f75069ee681405c7f6d4d4f2128f47703c3
root <root@localhost.localdomain>
2016-11-05 16:17:40 -0400

    added script

--- test.py
+++ test.py
@@ -7,9 +7,12 @@ for key, pkts in rdpcap(pcap).sessions().items():
     for pkt in pkts:
         if DNS in pkt:
             if pkt.ancount > 0 and isinstance(pkt.an, DNSRR):
-                response = base64.b64decode(pkt.an.rdata.decode('ASCII'))
+                data = pkt.an.rdata.decode('ASCII')
+                decoded = base64.b64decode(data)
+                print (decoded)
+                continue
                 if "FILE:" in response:
-                    print("%s" % (base64.b64decode(pkt.an.rdata.decode('ASCII'))))
+                    #print("%s" % (base64.b64decode(pkt.an.rdata.decode('ASCII'))))
                     pitcher += response.split("FILE:")[1]
 
 #print(pitcher)
